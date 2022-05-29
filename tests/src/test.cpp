0f728eeffcc51643a8b8e9c5ab4ed8bdfa6f2980
Takayama Fumihiko <tekezo@pqrs.org>
2022-05-29 10:19:24 +0900

    catchorg/Catch2 -> boost-ext/ut

--- tests/src/test.cpp
+++ tests/src/test.cpp
@@ -1,20 +1,25 @@
-#define CATCH_CONFIG_MAIN
-#include <catch2/catch.hpp>
-
+#include <boost/ut.hpp>
 #include <pqrs/sign.hpp>
 
-TEST_CASE("sign") {
-  REQUIRE(pqrs::make_sign(100) == pqrs::sign::plus);
-  REQUIRE(pqrs::make_sign(1) == pqrs::sign::plus);
-  REQUIRE(pqrs::make_sign(1.5f) == pqrs::sign::plus);
-  REQUIRE(pqrs::make_sign(1.5) == pqrs::sign::plus);
+int main(void) {
+  using namespace boost::ut;
+  using namespace boost::ut::literals;
 
-  REQUIRE(pqrs::make_sign(0) == pqrs::sign::zero);
-  REQUIRE(pqrs::make_sign(0.0f) == pqrs::sign::zero);
-  REQUIRE(pqrs::make_sign(0.0) == pqrs::sign::zero);
+  "sign"_test = [] {
+    expect(pqrs::make_sign(100) == pqrs::sign::plus);
+    expect(pqrs::make_sign(1) == pqrs::sign::plus);
+    expect(pqrs::make_sign(1.5f) == pqrs::sign::plus);
+    expect(pqrs::make_sign(1.5) == pqrs::sign::plus);
 
-  REQUIRE(pqrs::make_sign(-100) == pqrs::sign::minus);
-  REQUIRE(pqrs::make_sign(-1) == pqrs::sign::minus);
-  REQUIRE(pqrs::make_sign(-1.5f) == pqrs::sign::minus);
-  REQUIRE(pqrs::make_sign(-1.5) == pqrs::sign::minus);
+    expect(pqrs::make_sign(0) == pqrs::sign::zero);
+    expect(pqrs::make_sign(0.0f) == pqrs::sign::zero);
+    expect(pqrs::make_sign(0.0) == pqrs::sign::zero);
+
+    expect(pqrs::make_sign(-100) == pqrs::sign::minus);
+    expect(pqrs::make_sign(-1) == pqrs::sign::minus);
+    expect(pqrs::make_sign(-1.5f) == pqrs::sign::minus);
+    expect(pqrs::make_sign(-1.5) == pqrs::sign::minus);
+  };
+
+  return 0;
 }
